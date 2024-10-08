df5999edd65ed445ea4be223fce1df1b3d01f87c
oliver-bardi <bardi.oliver@student.ms.sapientia.ro>
2024-10-08 22:25:50 +0300

    First Commit

---
+++ kotlin_lab/Lab_3/src/DictionaryProvider.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+
+class DictionaryProvider {
+    companion object{
+        fun createDictionary(type: DictionaryType): IDictionary {
+            return when(type){
+                DictionaryType.ARRAY_LIST -> ListDictionary
+                DictionaryType.TREE_SET -> TreeSetDictionary
+                DictionaryType.HASH_SET -> HashSetDictionary
+            }
+        }
+    }
+}
